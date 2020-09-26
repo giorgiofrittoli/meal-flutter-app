d4ae1d1886f006e609b7dedf8472d09a6f685025
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-09-26 10:52:47 +0200

    base catgory screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,8 +1,26 @@
 import "package:flutter/material.dart";
+import 'package:flutter_complete_guide/category_item.dart';
 
-class CatgoryScreen extends StatelessWidget {
+import "dummy_data.dart";
+
+class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text("DeliMeals"),
+      ),
+    );
   }
 }
