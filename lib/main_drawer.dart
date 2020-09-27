448b9d246fc6a33b9b0648a8b0d796e4c89e7ce1
Giorgio Frittoli <giorgio.frittoli@gmail.com>
2020-09-27 10:05:30 +0200

    fix navigation stack

--- lib/main_drawer.dart
+++ lib/main_drawer.dart
@@ -43,9 +43,12 @@ class MainDrawer extends StatelessWidget {
             height: 20,
           ),
           _buildListTile("Meals", Icons.restaurant,
-              () => Navigator.of(context).pushNamed("/")),
-          _buildListTile("Filters", Icons.settings,
-              () => Navigator.of(context).pushNamed(FiltersScreen.routeName)),
+              () => Navigator.of(context).pushReplacementNamed("/")),
+          _buildListTile(
+              "Filters",
+              Icons.settings,
+              () => Navigator.of(context)
+                  .pushReplacementNamed(FiltersScreen.routeName)),
         ],
       ),
     );
--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,10 +1,17 @@
 import "package:flutter/material.dart";
 
+import '../main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static const routeName = "filters-screen";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Filters"),
+      ),
+      drawer: MainDrawer(),
+    );
   }
 }
